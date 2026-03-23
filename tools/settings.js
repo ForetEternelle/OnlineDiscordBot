a0b85844c7bf9f6be40a15a237120980b84835a0
Arnaud Michel <contact@arnaudmichel.fr>
2026-03-23 22:40:52 +0100

    feat: refactor moveInfo and pokemonInfo functions to include client parameter and improve logging

--- tools/settings.js
+++ tools/settings.js
@@ -15,14 +15,13 @@ const botName = getEnv('DISCORD_BOT_NAME', ['BOT_NAME']) || 'PSDK Bot';
 const botVersion = getEnv('DISCORD_BOT_VERSION', ['BOT_VERSION']) || '1.0';
 const discordToken = getEnv('DISCORD_TOKEN', ['TOKEN']);
 const discordClientId = getEnv('DISCORD_CLIENT_ID', ['CLIENT_ID']);
+const logsChannelId = getEnv('DISCORD_LOGS_CHANNEL_ID');
 const onlineServerBearerToken = getEnv('ONLINE_SERVER_BEARER_TOKEN', ['BEARER']);
 const baseUrlOnlineServerAPI = getEnv('ONLINE_SERVER_API_URL', ['ONLINE_SERVER_URL']);
 const baseUrlDataApi = getEnv('DATA_API_BASE_URL', ['BASE_URL_DATA_API']);
 const pokemonImageBaseUrl = getEnv('POKEMON_IMAGE_BASE_URL', ['IMAGE_URL']);
 // Color of the messages
 const embedColor = 0x345C6D;
-// Name of the logs channel
-const logsChannelName = '📰-logs';
 
 const intents = [
     GatewayIntentBits.Guilds,
@@ -39,6 +38,6 @@ module.exports = {
     baseUrlDataApi,
     pokemonImageBaseUrl,
     embedColor,
-    logsChannelName,
+    logsChannelId,
     intents
 };
